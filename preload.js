c8233d41d3ea6f88213310c701c7e96157b3c0b2
Homura <egzozu.be1.bas@gmail.com>
2025-11-15 15:47:30 +0300

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,11 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 process.once('loaded', () => {
-  contextBridge.exposeInMainWorld('steam', true); // required so Wolvesville will think it's launched though Steam Client.
+  contextBridge.exposeInMainWorld('steam', true); // required so Wolvesville will think it's launched through Steam Client.
   contextBridge.exposeInMainWorld('sendSteamIpc', ({ action, payload }) => {
     if (action === 'EXIT_GAME') {
       ipcRenderer.send('EXIT_GAME');
     }
   });
 });
+
